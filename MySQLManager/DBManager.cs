f813581bacf22ec51810e0cbd63292d2d80fac81
beutchowski <allopalto@icloud.com>
2021-04-17 22:44:26 +0200

    DBManager : OpenConnexion OK, CloseConnexion OK, CreateTable OK.

--- MySQLManager/DBManager.cs
+++ MySQLManager/DBManager.cs
@@ -9,23 +9,22 @@ namespace MySQLManager
 {
     class DBManager
     {
-        private static MySqlConnection connection;
+        public static MySqlConnection connection;
 
 
 
         /// <summary>
-        /// Fonction qui se charge d'établir la connection à la base de données locale
+        /// Fonction qui se charge d'établir la connection à la base de données
         /// </summary>
         /// <param name="databaseName">Nom de la base de données</param>
         /// <param name="userName"></param>
         /// <param name="password"></param>
-        static public void ConnectToDB(string server, string databaseName,
+        static public void ConnectToDB(string server, int port, string databaseName,
                                                            string userName,
                                                            string password)
         {
-            string connectionString = $"server={server};port=3306;database={databaseName};user={userName};password={password};";
-
-            MySqlConnection connection = new MySqlConnection(connectionString);
+            string connectionString = $"server={server};port={port};database={databaseName};user={userName};password={password};";
+            connection = new MySqlConnection(connectionString);
 
             try
             {
@@ -37,6 +36,8 @@ namespace MySQLManager
                 Console.WriteLine(ex.Message);
             }
         }
+
+
         /// <summary>
         /// 
         /// </summary>
@@ -46,15 +47,33 @@ namespace MySQLManager
             return connection;
         }
 
-        static public void CloseConnection()
+        /// <summary>
+        /// Termine la connexion à la base de données
+        /// </summary>
+        /// <param name="connection"></param>
+        static public void CloseConnection(MySqlConnection connection)
         {
             if (connection != null)
             {
                 connection.Close();
                 connection = null;
             }
-
             Console.WriteLine("Connection fermée");
+        }
+
+        /// <summary>
+        /// Créer une table pour stocker des données dont la langue de saisie est le français (é, à, etc.)
+        /// Deux colonnes sont créées, un id et une première colonne
+        /// </summary>
+        static public void CreateTable()
+        {
+            MySqlCommand Create_table = new MySqlCommand(@"
+            CREATE TABLE `TABLE1` 
+            (id INT NOT NULL AUTO_INCREMENT,
+            colonne1 VARCHAR(15) NOT NULL, 
+            PRIMARY KEY (id)) COLLATE='utf8_roman_ci';", connection); 
+            //Ou peut-être 'utf8_general_ci', à voir comment la table gère les accents en français...
+            Create_table.ExecuteNonQuery();
         }
 
 
